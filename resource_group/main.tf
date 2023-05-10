dbd6904aa8a38e3ae53f388c11f134a5c28fdabb
ryanmcintosh-hitachi <127242195+ryanmcintosh-hitachi@users.noreply.github.com>
2023-05-10 12:22:02 -0500

    Delete main.tf

--- resource_group/main.tf
+++
@@ -1,9 +0,0 @@
-# Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "myrg" {
-    name     = "myResourceGroup"
-    location = "${var.location}"
-
-    tags {
-        environment = "Terraform Demo"
-    }
-}
